bb0b3029e694bf52b6b8c121fdff5269f7089ca4
Alloffices <alloffices.nyc@gmail.com>
2015-07-21 09:01:51 -0400

    add registration and name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721000455) do
+ActiveRecord::Schema.define(version: 20150721120917) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20150721000455) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.boolean  "admin"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
