f266f77ef566474783b1becbcd47a308b1b68c49
Alloffices <alloffices.nyc@gmail.com>
2015-07-20 20:15:18 -0400

    added paperclip gem

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
 	
 	validates :title, :content, presence: true
 
-	has_attached_file :image, styles: { medium: "750x500#", thumb: "350x350#" }
+	has_attached_file :image, styles: { medium: "700x500#", small: "350x350#" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 	belongs_to :user
