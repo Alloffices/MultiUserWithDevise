8978d2d7ba6eb5f4969d1ce7c427d41b3dff9959
Alloffices <alloffices.nyc@gmail.com>
2015-07-22 22:17:26 -0400

    changed the index page

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
 	
 	validates :title, :content, presence: true
 
-	has_attached_file :image, styles: { medium: "700x500#", small: "350x350#" }
+	has_attached_file :image, styles: { medium: "200x200#", small: "300x300#" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 	belongs_to :user
